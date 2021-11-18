0879cf9d4bfd637d3b4894cef77fe976393a8f15
Gogs <14806317+htmambo@users.noreply.github.com>
2021-11-19 02:41:43 +0800

    调整导出PDF的样式。

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -147,7 +147,7 @@
                 if($('.wz-panel-breadcrumb').length) {
                     pos = $('.wz-panel-right').offset();
                     let w = $(window).width() - pos.left + 'px';
-                    $('.wz-panel-breadcrumb').css({'height':'48px', 'width':w,'position':'fixed', 'z-index':'999999'});
+                    $('.wz-panel-breadcrumb').css({'height':'48px', 'width':w,'position':'fixed', 'z-index':'99'});
                     $('.wz-project-main').first().css({'padding-top':'58px'});
                     $('.wz-panel-right').height(h - pos.top).css({'overflow-y':'auto'});
                 } else {
