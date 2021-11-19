9eed13db73ebfe2bc35c257df13217bfd45d8557
Gogs <14806317+htmambo@users.noreply.github.com>
2021-11-19 20:45:39 +0800

    修复模板中js文件的引用。

--- resources/views/components/markdown-show.blade.php
+++ resources/views/components/markdown-show.blade.php
@@ -17,7 +17,7 @@
 <script src="{{ cdn_resource('/assets/vendor/editor-md/lib/sequence-diagram.min.js') }}"></script>
 <script src="{{ cdn_resource('/assets/vendor/editor-md/lib/flowchart.min.js') }}"></script>
 <script src="{{ cdn_resource('/assets/vendor/editor-md/lib/jquery.flowchart.min.js') }}"></script>
-<script src="{{ cdn_resource('/assets/vendor/mermaid.min.js') }}"></script>
+<script src="{{ cdn_resource('/assets/vendor/mermaid/mermaid.min.js') }}"></script>
 <script src="{{ cdn_resource('/assets/vendor/editor-md/editormd.js') }}?{{ resourceVersion() }}"></script>
 <script src="{{ cdn_resource('/assets/vendor/viewer/viewer.min.js') }}"></script>
 
