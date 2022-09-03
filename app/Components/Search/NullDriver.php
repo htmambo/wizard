4e3e588e108fb09308a393aad4e7b5da0bad7deb
hoping <14806317+htmambo@users.noreply.github.com>
2022-09-03 14:46:58 +0800

    Null搜索引擎改用数据库全文搜索

--- app/Components/Search/NullDriver.php
+++ app/Components/Search/NullDriver.php
@@ -3,6 +3,7 @@
 namespace App\Components\Search;
 
 use App\Repositories\Document;
+use Illuminate\Support\Facades\Log;
 
 /**
  * 基于数据库的搜索
@@ -22,6 +23,34 @@ class NullDriver implements Driver
 
     public function search(string $keyword, int $page, int $perPage): ?Result
     {
+        try{
+            $documentModel = Document::query();
+            // 先按手动分词处理
+            $keywords = explode(' ', $keyword);
+            if(count($keywords) == 1) {
+                $documentModel->where('title', 'like', "%{$keyword}%", 'or');
+                $documentModel->where('content', 'like', "%{$keyword}%", 'or');
+            } else {
+                $documentModel->orWhere(function ($query) use($keywords) {
+                    foreach($keywords as $keyword) {
+                        $query->where('title', 'like', "%{$keyword}%");
+                    }
+                });
+                $documentModel->orWhere(function ($query) use($keywords) {
+                    foreach($keywords as $keyword) {
+                        $query->where('content', 'like', "%{$keyword}%");
+                    }
+                });
+            }
+            $ids = $documentModel->select('id')->get()->toArray();
+            return new Result(array_column($ids, 'id'),
+                $keywords,
+                count($ids)
+            );
+        } catch (\Exception $ex) {
+            Log::error('search failed', ['message' => $ex->getMessage()]);
+        }
+
         return null;
     }
 }
