6a10dc076d3c7e37b35bd2ae097fff6c63bf2635
hoping <htmambo@gmail.com>
2023-08-20 02:37:21 +0800

    修复migrate脚本的文件名

--- database/migrations/2021_11_29_133806_add_page_html_code.php
+++ database/migrations/2021_11_29_133806_add_page_html_code.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPageShareControl extends Migration
+class AddPageHtmlCode extends Migration
 {
     /**
      * Run the migrations.
